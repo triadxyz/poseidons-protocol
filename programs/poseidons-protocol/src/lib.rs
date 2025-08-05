9a7aee4d99c04fb564f8a730eb3a37a306a70b44
Mateus Barbosa <mateus@triadmarkets.com>
2025-08-05 14:30:44 -0300

    logic was introduced for the rabbit game, current variables will be replaced

--- programs/poseidons-protocol/src/lib.rs
+++ programs/poseidons-protocol/src/lib.rs
@@ -5,10 +5,66 @@ declare_id!("E65MHJjJT2ihTLgMfHdWtsNYMA6iJsCjraf7UrxU2sDP");
 #[program]
 pub mod poseidons_protocol {
     use super::*;
+    use rand::Rng;
+    
+    //imports random number generator and generates a random number between 0 and 36
+
+
+
+
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
+
+    }
+    pub fn game(ctx: Context<Initialize>) -> Result<()> {
+        let mut rng = rand::thread_rng();
+        let random_number: u8 = rng.gen_range(0..37);
+        let mut user: Vec<&str> = Vec::new();//for test purposes, identifies the user
+        let mut betValue: Vec<f64> = Vec::new();
+        let mut chosenHole: Vec<i32> = Vec::new();
+        let mut gainedValue: Vec<f64> = Vec::new();//will determine the value that was obtained
+        let mut cont = 0;
+        let mut rabbitHole: i32;
+        let mut totalBetLost = 0.0;
+        let mut totalBetWon = 0.0;
+        let mut multiplier : f64;
+        user.push("a1");
+        betValue.push(12.66);
+        chosenHole.push(1);
+        cont += 1;
+        user.push("b2");
+        betValue.push(12.66);
+        chosenHole.push(2);
+        cont += 1;
+        user.push("c3");
+        betValue.push(0.31);
+        chosenHole.push(3);
+        cont+= 1;
+        rabbitHole = random_number;
+        println!("The rabbit has chosen the {}° hole", rabbitHole);
+        for i in 0.. cont{
+           if chosenHole[i] == rabbitHole{
+                totalBetWon +=  betValue[i];
+                println!("User {} has won", user[i]);
+            }else{
+                totalBetLost += betValue[i];
+                println!("User {} has lost", user[i]);
+            }
+        }
+        println!("ok");
+        multiplier = totalBetLost/totalBetWon;
+        for i in 0..cont{
+            if chosenHole[i] == rabbitHole{
+              gainedValue[i] = (betValue[i]*multiplier)+betValue[i];/*makes it so winning users
+              always get their entry ammount back + multiplier*/
+             println!("User {} won R${}",user[i],gainedValue[i]);
+            
+            }else{
+              gainedValue[i] = 0.0;
+            }
+        }
     }
 }
 
